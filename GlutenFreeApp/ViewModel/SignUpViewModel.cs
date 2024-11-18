ba9f9275d0335ffdf1c063a61577d0655e578cdb
User <User@LAB2-2>
2024-11-18 15:41:08 +0200

    continue signup

--- GlutenFreeApp/ViewModel/SignUpViewModel.cs
+++ GlutenFreeApp/ViewModel/SignUpViewModel.cs
@@ -1,9 +1,12 @@
 using GlutenFreeApp.ViewModel;
 using System.Windows.Input;
+using GlutenFreeApp.Services;
+using GlutenFreeApp.Models;
 namespace GlutenFreeApp.ViewModel;
 
 public class SignUpViewModel : ViewModelBase
 {
+    //התכונות
     private string username;
     public string Username
     {
@@ -29,6 +32,9 @@ public class SignUpViewModel : ViewModelBase
             passError = value; OnPropertyChanged();
         }
     }
+
+
+    //ולידציה לסיסמה
 
     private string password;
     public string? Password
@@ -89,8 +95,64 @@ public class SignUpViewModel : ViewModelBase
     //נחבר את זה לכפתורים
     public ICommand ManagerSelectedCommand { get; set; }
     public ICommand VisitorSelectedCommand { get; set; }
+    public ICommand SubmitCommand { get; set; }
 
-    
+    private GlutenFreeServiceWebAPIProxy proxy;
+    public SignUpViewModel(GlutenFreeServiceWebAPIProxy proxy)
+    {
+        //הפעולות שמשנות את האנטריז בהתאם
+
+        this.proxy = proxy;
+        ManagerSelectedCommand = new Command(ManagerSelected, () => !IsManager);
+        VisitorSelectedCommand = new Command(VisitorSelected, () => IsManager);
+        SubmitCommand = new Command(OnRegister);
+        IsManager = false;
+
+    }
+
+
+    //ADD MODELSBL AND THE NEEDED THINGS BELOW
+
+
+
+
+    private async void OnRegister()
+    {
+
+        var newUser = new UsersInfo
+        {
+            Name = this.Username,
+            Password = this.Password,
+            //need to add the actions in ModelsBl
+            TypeID = this.GetType().Name,
+            UserID = this.GetID.Name
+        };
+
+        //the server is in action
+        InServerCall = true;
+        //call the proxy and the register with new user
+        newUser = await proxy.Register(newUser);
+        //we're done with the server call so enable
+        InServerCall=false;
+
+        //check if registeration okay
+        if (newUser != null)
+        {
+            InServerCall = false;
+            await Application.Current.MainPage.DisplayAlert("Registration", "User Data Was Saved", "ok");
+
+            InServerCall = false;
+            ((App)(Application.Current)).MainPage.Navigation.PopAsync();
+        }
+        else 
+        {
+            //display not good alert
+            string errorMsg = "Registration failed. Please try again.";
+            await Application.Current.MainPage.DisplayAlert("Registration", errorMsg, "ok");
+        }
+
+    }
+
 
     //נחבר את זה אם המנהל הוא האופציה הנבחרת
     private bool isManager;
@@ -106,22 +168,10 @@ public class SignUpViewModel : ViewModelBase
         }
     }
 
-    public SignUpViewModel()
-    {
-     
-        ManagerSelectedCommand = new Command(ManagerSelected,()=>!IsManager);
-        VisitorSelectedCommand = new Command(VisitorSelected,()=>IsManager);
-        IsManager = false;
-
-    }
-
-   
-
-
+    
     private async void ManagerSelected()
     {
-        IsManager=true;
-        
+        IsManager=true; 
     }
 
     private async void VisitorSelected()
