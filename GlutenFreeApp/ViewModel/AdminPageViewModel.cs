2c74000d96ecb68ed3df1a4853444eb53821dc9b
User <User@LAB2-2>
2024-12-02 15:27:10 +0200

    add binding to admin and information

--- GlutenFreeApp/ViewModel/AdminPageViewModel.cs
+++ GlutenFreeApp/ViewModel/AdminPageViewModel.cs
@@ -3,11 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using GlutenFreeApp.Services;
+
+
 
 namespace GlutenFreeApp.ViewModel
 {
     public class AdminPageViewModel:ViewModelBase
     {
+        //to do:
+        //do a command that adds the fact the admin wants to add to the DB
+        //do a sort of table that shows all of the pending restaurants and allow the admin to approve them
+        //order: first the facts and then do the table - example might be in talsi's git with the monkeys
+
+        //ADD BINDING
+
+        private GlutenFreeServiceWebAPIProxy proxy;
+        private IServiceProvider serviceProvider;
+
+        #region builder
+        public AdminPageViewModel(GlutenFreeServiceWebAPIProxy proxy, IServiceProvider serviceProvider) 
+        {
+            AddFactCommand = new Command(AddFact);
+        }
+        #endregion
+
+        #region properties
+        private string fact;
+        public string Fact 
+        {
+            get => fact;
+            set 
+            {
+                if (fact!=value)
+                {
+                    fact = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+
+
+        #region buttons
+        public ICommand AddFactCommand { get; set; }
+        #endregion
+
+        private async void AddFact()
+        {
+            //add fact to the data base 
+        }
 
     }
 }
--- GlutenFreeApp/ViewModel/InformationViewModel.cs
+++ GlutenFreeApp/ViewModel/InformationViewModel.cs
@@ -9,6 +9,11 @@ namespace GlutenFreeApp.ViewModel
 {
     public class InformationViewModel:ViewModelBase
     {
+        //to do:
+        //show the facts in a label that will rotate every few seconds in an animated way
+        //take the facts from the information table in the DB in a random order
+
+
         private GlutenFreeServiceWebAPIProxy proxy;
         private IServiceProvider serviceProvider;
         #region builder
